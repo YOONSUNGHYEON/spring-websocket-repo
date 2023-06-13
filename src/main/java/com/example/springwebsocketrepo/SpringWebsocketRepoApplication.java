52465ede261f4ccd9bd726e5af7256308f1af155
YOONSUNGHYEON <qead753951@naver.com>
2023-06-13 20:40:57 +0900

    Initial commit

---
+++ src/main/java/com/example/springwebsocketrepo/SpringWebsocketRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocketrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springwebsocketrepo/SpringWebsocketRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocketrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
